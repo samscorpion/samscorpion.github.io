8bff72e2dc8b2e4ba1446aba6e01ba2d13015e66
samscorpion <samscorpion@gmail.com>
2021-03-11 21:13:04 -0500

    .

--- js/menu-mobil.js
+++ js/menu-mobil.js
@@ -1,12 +1,10 @@
 
-// const menu_mobil = document.querySelector(".menu-mobil");
+const menu_mobil = document.querySelector("#menu-mobil");
 // const menu = document.querySelector(".menu");
 // const lista_menu = document.querySelector(".menu ul");
 
-// menu_mobil.addEventListener("click", e =>{
+menu_mobil.addEventListener("click", e =>{
    
-//     menu.style.zIndex = "10011";
-
+    menu_mobil.classList.toggle("open");
     
-    
-// })
+})
